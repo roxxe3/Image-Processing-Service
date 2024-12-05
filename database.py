cdce0f334c23f30bf2a88b044c56b29550fadc6e
roxxe3 <hamzafarissi@gmail.com>
2024-12-05 23:46:11 +0100

    Update database connection to use SQLite file and integrate SQLAlchemy for user management

--- database.py
+++ database.py
@@ -4,7 +4,8 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
 
 
-engine = create_engine("sqlite://", echo=True)
+
+engine = create_engine("sqlite:///mydatabase.db", echo=True)
 Base = declarative_base()
 
 class User(Base):
--- main.py
+++ main.py
@@ -2,15 +2,21 @@ from fastapi import FastAPI
 from models.user import create_user, UserLogin
 from auth.auth import encode_jwt, decode_jwt
 import pymongo
+from sqlalchemy.orm import sessionmaker
+from database import engine, User
 
-
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-
-mydb = myclient["mydatabase"]
-users_list= mydb["users"]
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 app=FastAPI()
+
+
+def add_user(username, password):
+    new_user = User(username=username, password=password)
+    session.add(new_user)
+    session.commit()
+    return True
 
 
 @app.get("/")
@@ -20,6 +26,7 @@ def hello():
 
 @app.post("/signup")
 async def signup(user: create_user):
+    add_user(user.username, user.password)
     return encode_jwt(user.username, user.password)
 
 if __name__ == "__main__":
