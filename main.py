24606bf17f38f31b8686a1e621e61ce62c3aaade
roxxe3 <hamzafarissi@gmail.com>
2024-12-06 00:56:33 +0100

    Add user login functionality with validation

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ def add_user(username, password):
     session.commit()
     return True
 
+def check_user(form: UserLogin):
+    users = session.query(User).all()
+    for user in users:
+        if user.username == form.username and user.password == form.password:
+            return True
+    return False
+
 
 @app.get("/")
 def hello():
@@ -28,6 +35,13 @@ def hello():
 async def signup(user: create_user):
     add_user(user.username, user.password)
     return encode_jwt(user.username, user.password)
+
+@app.post("/login")
+async def user_login(form: UserLogin):
+    if check_user(form):
+        return encode_jwt(form.username, form.password)
+    else:
+        return {"message": "invalide login"}
 
 if __name__ == "__main__":
     app.run()
