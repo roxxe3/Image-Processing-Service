49db28591e00235e4b5562cbcf14b6359a337f25
roxxe3 <hamzafarissi@gmail.com>
2024-12-12 14:50:24 +0100

    Refactor process_image_from_url function to accept width and height parameters for resizing

--- process_image.py
+++ process_image.py
@@ -3,11 +3,20 @@ import requests
 from io import BytesIO
 from image import upload_transformed_image
 
-async def process_image_from_url(url, transformation):
+async def process_image_from_url(url, width, height):
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
-    if transformation.resize:
-        img_resized = img.resize((transformation.resize.width, transformation.resize.height))
-        img_resized.show()
-        await upload_transformed_image(url, transformation)
+    img_resized = img.resize((width, height))
+    img_resized.show()
+    
+
+
+
+
+
+
+process_image_from_url("https://imagesbucketpillow.s3.eu-west-3.amazonaws.com/1712076948499.jpeg", 100, 200)
+
+
+    
 
