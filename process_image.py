bf3ce6b65b6ec2ab23421edc1154f710fadb9362
roxxe3 <hamzafarissi@gmail.com>
2024-12-13 19:30:32 +0100

    Remove unused import statement for image upload in process_image.py

--- process_image.py
+++ process_image.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import requests
 from io import BytesIO
-from image import upload_transformed_image
 
 async def process_image_from_url(url, width, height):
     response = requests.get(url)
